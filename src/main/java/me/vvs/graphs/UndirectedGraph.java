f41a2260e0f5f9fae9a0b4e6748c29343d77fc96
vvs14 <vivekvsingh14@gmail.com>
2024-10-05 17:53:33 +0530

    [BFS]

--- src/main/java/me/vvs/graphs/UndirectedGraph.java
+++ src/main/java/me/vvs/graphs/UndirectedGraph.java
@@ -1,2 +1,35 @@
-package me.vvs.graphs;public class UndirectedGraph {
+package me.vvs.graphs;
+
+import java.util.*;
+
+public class UndirectedGraph {
+    private Map<String, List<String>> adjList;
+
+    public UndirectedGraph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addEdge(String src, String dest) {
+        if (!adjList.containsKey(src)) adjList.put(src, new ArrayList());
+        adjList.get(src).add(dest);
+        if (!adjList.containsKey(dest)) adjList.put(dest, new ArrayList());
+        adjList.get(dest).add(src);
+    }
+
+    public List<String> getNeighbours(String vertex) {
+        return adjList.getOrDefault(vertex, Collections.emptyList());
+    }
+
+    public String toString() {
+        return adjList.toString();
+    }
+
+    public Optional<String> getAVertex() {
+        Optional<String> firstKeyOptional = adjList.keySet().stream().findFirst();
+        return firstKeyOptional;
+    }
+
+    public List<String> getAllVertices() {
+        return new ArrayList<>(adjList.keySet());
+    }
 }
